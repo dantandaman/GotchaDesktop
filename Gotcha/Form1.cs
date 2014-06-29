8d1b7a8994b7cb30a8707255d6cd0609dbb69c16
amitofsk <amamitof7@gmail.com>
2014-06-28 21:38:56 -0400

    Power law check funcs almost working

--- Gotcha/Form1.cs
+++ Gotcha/Form1.cs
@@ -17,8 +17,13 @@ namespace Gotcha
         public List<TransactionInfo> _records;
        
         int maxtrans;
-        
-
+        int[] mostsigdigs = new int[1000];
+        float[] price = new float[1000];
+        int[] distrdigs = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        float erro, worstcase;
+        float[] percdigs = new float[10];
+        float[] percdigsexp = {(float)30.1,(float)17.6,(float)12.5,(float)9.7,(float)7.9,(float)6.7,(float)5.8,(float)5.1,(float)4.6};
+        float wctolerance=(float)0.009;
 
         public GotchaWindow()
         {
@@ -30,7 +35,7 @@ namespace Gotcha
         private void SetupStuff()
         {
             _records = new List<TransactionInfo>();
-            FilterComboBox.Items.AddRange(new string[]{"Less Than $1", "Over $100", "International"});
+            FilterComboBox.Items.AddRange(new string[]{"Less Than $1", "Over $100", "International","Benford's law check"});
         }
 
         //I will shift this code out once I get the API to read from PayPal done
@@ -76,6 +81,12 @@ namespace Gotcha
         {
             if (FilterComboBox.Text.Contains("Less Than $1"))
                 LessThanADollar();
+            if(FilterComboBox.Text.Contains("law"))
+            { //call the two related functions 
+              mostsigdig();
+         	  distrdig();
+
+            }
         }
 
         private void LessThanADollar()
@@ -107,6 +118,7 @@ namespace Gotcha
 
         private void mostsigdig()
         {
+            //This function is used in the power law check. It picks off the most sig. digit.
             int i, msd;
             for (i = 0; i < maxtrans; i++)
             {
@@ -121,13 +133,44 @@ namespace Gotcha
                     mostsigdigs[i] = msd % 10;
                     msd = msd / 10;
                 }
-                printf(" MSD is %d \n", mostsigdigs[i]);
-            }
+            //    printf(" MSD is %d \n", mostsigdigs[i]);
 
+
+            }
+            
         }
 
         private void distrdig()
         {
+            //This function is also used in the power law check.
+
+            int i;
+
+            for (i = 0; i < maxtrans; i++)
+                distrdigs[mostsigdigs[i]]++;
+            erro = 0;
+          //  printf(" Distribution of digits are \n");
+            for (i = 0; i < 10; i++)
+            {
+                percdigs[i] = (float)(distrdigs[i]) * (float)100.0 / (float)maxtrans;
+                erro = erro + (percdigs[i] - percdigsexp[i]) * (percdigs[i] - percdigsexp[i]);
+            //    printf(" %d  ->  %d  -> %f\n", i, distrdigs[i], percdigs[i]);
+            }
+            worstcase = 0;
+            for (i = 0; i < 9; i++)
+                worstcase = worstcase + percdigsexp[i] * percdigsexp[i];
+            worstcase = worstcase + (100 - (float)4.58) * (100 - (float)4.58);
+
+            if(worstcase < wctolerance)
+            {
+              //We suspect fraud in the entire list. We need to give some type of warning
+                //I'm going to try to highlight the entire first column in orange
+          
+
+                for(int ii=1;ii<maxtrans;i++)
+                {BaseGridView.Rows[ii].Cells[1].Style = new DataGridViewCellStyle { BackColor = Color.Orange };
+                }
+            }
 
         }
 
